886ee8516ac4be7432b5f86ebd8d0db3a54cc4ce
mathlun <matheuslunadev@gmail.com>
2022-09-13 13:39:43 -0300

    Feature: Add Header Component

---
+++ src/components/Header.svelte
@@ -0,0 +1,38 @@
+
+<header>
+    <div>
+
+    </div>
+    <div class="brand">
+        <h2>Svelte</h2>
+        <h3>Ecommerce</h3>
+    </div>
+    <div class="auth">
+        <p>Login | Cadastre-se</p>
+    </div>
+</header>
+
+<style>
+    header, header > div {
+        display: flex;
+    }
+
+    header {
+        padding: 5px 10px;
+        justify-content: space-between;
+        border: 1px solid #ddd;
+    }
+
+    header > div {
+        padding: 0;
+    }
+
+    header > .brand > h2 {
+        color: orangered;
+    }
+
+    header > .brand > h3 {
+        color: #ddd;
+        margin-left: 2px;
+    }
+</style>
